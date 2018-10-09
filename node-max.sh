e5a86e93df4cd3f2eff15523aae12e185e2a322f
miy <>
2018-10-09 21:57:33 +0900

    add node-max.sh, npm-max.sh
    usage: $ ./npm-max.sh install, and so on.
    (to be sure to match Max8's node, npm version, but it depends on MacOS)

---
+++ node-max.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+path="/Applications/Max.app/Contents/Resources/C74/packages/Node\ For\ Max/source/bin/osx/node"
+eval $path $*
---
+++ npm-max.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+path="/Applications/Max.app/Contents/Resources/C74/packages/Node\ For\ Max/source/bin/osx/npm"
+eval $path $* --scripts-prepend-node-path
